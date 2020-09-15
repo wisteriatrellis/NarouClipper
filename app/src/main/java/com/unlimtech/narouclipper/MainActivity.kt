d858edd999ea9578eee9b26e3e9daf8c63d1b349
fujidana <fujidanawt@gmail.com>
2020-09-15 21:20:19 +0900

    add clip button

--- app/src/main/java/com/unlimtech/narouclipper/MainActivity.kt
+++ app/src/main/java/com/unlimtech/narouclipper/MainActivity.kt
@@ -2,10 +2,18 @@ package com.unlimtech.narouclipper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text: TextView = findViewById<TextView>(R.id.textView) as TextView
+        val clipButton: Button = findViewById<Button>(R.id.clipButton) as Button
+        clipButton.setOnClickListener {
+            text.text = "change!"
+        }
     }
 }
